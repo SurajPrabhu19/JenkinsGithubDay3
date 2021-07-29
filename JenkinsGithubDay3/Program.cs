63bfb1d974f70e4d9f02d25a842a60969448af8b
SurajPrabhu19 <surajprabhu19@gmail.com>
2021-07-29 10:00:26 +0530

    Add project files.

---
+++ JenkinsGithubDay3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JenkinsGithubDay3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
